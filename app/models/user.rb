2192db9217630e4eecdcdf9bf373016875e09c72
Andrii Kovalchuk <andrii.kovalchuk@just-eat.com>
2021-06-26 15:16:46 +0300

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-
   devise :database_authenticatable,
          :jwt_authenticatable,
          :registerable,
+         :validatable,
          jwt_revocation_strategy: JwtDenylist
+
 end
